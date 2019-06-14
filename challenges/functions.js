6c1d9afbf7d8053c0c6a873f7ced6f6dd05c6cba
TheIllusiveC4 <gasingdong@gmail.com>
2019-06-14 08:45:42 -0700

    finish closures

--- challenges/functions.js
+++ challenges/functions.js
@@ -43,7 +43,7 @@ console.log(consume('Mary', 'Poppins', greeting)); // Hello Mary Poppins, nice t
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation:
+// Explanation: Inner functions have access to variables in parent functions.
 
 const external = "I'm outside the function";
 
